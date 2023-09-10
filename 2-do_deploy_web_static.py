8f867bdb71d6198a8bc897064933825a4f0da7f3
OsiroA <osiroasunde@gmail.com>
2023-09-10 13:29:06 -0700

    this script distributes an archive to your web servers using the function do_deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+this fabric script uses the previous one created to distribute an archive
+to teh web-servers, using the function do_deploy
+"""
+from os import path
+from fabric.api import run, env, put
+env.hosts = ['54.237.88.25', '54.90.40.27']
+env.user = "ubuntu"
+
+
+def do_deploy(archive_path):
+    """This distributes a .tgz archive through web servers"""
+    if path.exists(archive_path):
+        archives = archive_path.split('/')[1]
+        archived_path = "/tmp/{}".format(archives)
+        a_folder = archives.split('.')[0]
+        folderPath = "/data/web_static/releases/{}/".format(a_folder)
+        put(archive_path, archived_path)
+        run("sudo mkdir -p {}".format(folderPath))
+        run("sudo tar -xzf {} -C {}".format(archived_path, folderPath))
+        run("sudo rm {}".format(archived_path))
+        run("sudo mv -f {}web_static/* {}".format(folderPath, folderPath))
+        run("sudo rm -rf {}web_static".format(folderPath))
+        run("sudo rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folderPath))
+        return True
+    else:
+        return False
