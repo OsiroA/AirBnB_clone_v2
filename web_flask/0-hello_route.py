1693fecefd9169999a04516a98b6ad9ffd52ca7a
OsiroA <osiroasunde@gmail.com>
2023-09-22 06:23:06 -0700

    pycodestyle fixes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,12 +7,14 @@ from flask import Flask
 
 oosi = Flask(__name__)
 
+
 @oosi.route('/', strict_slashes=False)
 def hello():
     """
     This function returns a requested string
     """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     oosi.run(host='0.0.0.0', port=5000)
