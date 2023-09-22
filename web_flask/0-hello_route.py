69d6989f7440612647b9213343d6bc643b4083b4
OsiroA <osiroasunde@gmail.com>
2023-09-22 06:17:59 -0700

    this is a script that starts a flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is a script that starts a Flask web application
+"""
+from flask import Flask
+
+
+oosi = Flask(__name__)
+
+@oosi.route('/', strict_slashes=False)
+def hello():
+    """
+    This function returns a requested string
+    """
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    oosi.run(host='0.0.0.0', port=5000)
