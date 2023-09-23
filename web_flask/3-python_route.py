11e8f99a9c6ccc716582b974c3167322372b8ae4
OsiroA <osiroasunde@gmail.com>
2023-09-23 03:57:27 -0700

    facing a slight bug, it is not printing on my terminal

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This is a script that starts a Flask web application
+"""
+from flask import Flask
+
+
+oosi = Flask(__name__)
+
+
+@oosi.route('/', strict_slashes=False)
+def hello():
+    """
+    This function returns a requested string
+    """
+    return 'Hello HBNB!'
+
+
+@oosi.route('/hbnb', strict_slashes=False)
+def helloHbnb():
+    """
+    This function displays just HBNB
+    """
+    return 'HBNB'
+
+
+@oosi.route('/c/<text>', strict_slashes=False)
+def cRoute(text):
+    '''
+    this displays C followed by the value of
+    the text variable
+    '''
+    return "C {}".format(text.replace("_", " "))
+
+
+@oosi.route('/python/', strict_slashes=False)
+@oosi.route('/python/<text>', strict_slashes=False)
+def pythonRoute(text="is cool"):
+    '''
+    This function replaces a given text
+    '''
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == '__main__':
+    oosi.run(host='0.0.0.0', port=5000)
