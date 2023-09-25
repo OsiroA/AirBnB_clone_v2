7994a69ecb3c398f9a4f6896c4d649b1a8ca276e
OsiroA <124213013+OsiroA@users.noreply.github.com>
2023-09-25 01:45:33 +0100

    Update 7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This starts a Flask web application.
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+oosi = Flask(__name__)
+
+
+@oosi.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+    Displays an HTML page with a list of all State objects in DBStorage.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@oosi.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    oosi.run(host="0.0.0.0")
